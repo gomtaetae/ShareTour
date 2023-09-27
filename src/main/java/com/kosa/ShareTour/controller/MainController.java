e4c834a21b56ac256fa59766e17ba3f15265139e
KOSA <dusdn4159@gmail.com>
2023-09-27 17:45:24 +0900

    package 페이지 추가(연우)

--- src/main/java/com/kosa/ShareTour/controller/MainController.java
+++ src/main/java/com/kosa/ShareTour/controller/MainController.java
@@ -2,12 +2,14 @@ package com.kosa.ShareTour.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class MainController {
 
     @GetMapping("/main")
-    public String page() {
+    public String page(){
         return "main";
+
     }
+
+
 }
