a14417683fe919693f14cc163fe6f503d7a050d5
KOSA <dusdn4159@gmail.com>
2023-10-02 16:37:02 +0900

    전 html.css수정

--- src/main/java/com/kosa/ShareTour/controller/MainController.java
+++ src/main/java/com/kosa/ShareTour/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.kosa.ShareTour.controller;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class MainController {
 
@@ -22,10 +19,22 @@ public class MainController {
     public String registerPage() {
         return "register";
     }
-}
 
+    @GetMapping("/auction")
+    public String mainAuction() {
+        return "auction";
+    }
     @GetMapping("/auctionplus")
     public String infoAuction() {
         return "auctionplus";
     }
+    @GetMapping("/con_product_page")
+    public String con_product() {
+        return "con_product_page";
+    }
+    @GetMapping("/package_tour")
+    public String tour() {
+        return "package_tour";
+    }
+
 }
