4c9dabb6fe7b2792dfca8f44e1d2ebc270514a3d
bagsangbeom <sangbeom1235@naver.com>
2023-10-05 16:34:38 +0900

    auction 페이지 및 상세 페이지 추가

---
+++ src/main/java/com/kosa/ShareTour/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kosa.ShareTour.repository;
+
+import com.kosa.ShareTour.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+
+}
