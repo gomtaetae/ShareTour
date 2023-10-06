656e30cd384d7e6e9e86baf4efb605693a849f22
bagsangbeom <sangbeom1235@naver.com>
2023-10-06 12:28:22 +0900

    auction 페이지 및 상세 페이지 추가

---
+++ src/main/java/com/kosa/ShareTour/repository/AccommodationRepository.java
@@ -0,0 +1,11 @@
+package com.kosa.ShareTour.repository;
+
+import com.kosa.ShareTour.entity.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface AccommodationRepository extends JpaRepository<Accommodation, Integer> {
+
+}
