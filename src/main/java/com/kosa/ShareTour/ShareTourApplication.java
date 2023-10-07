b6b7cc5421ed632edd54502b0bc94d098104512a
gomtaetae <rlaxodls108@hanmail.net>
2023-10-07 14:39:50 +0900

    spring security 로그인 일시정지

--- src/main/java/com/kosa/ShareTour/ShareTourApplication.java
+++ src/main/java/com/kosa/ShareTour/ShareTourApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+//exclude를 통해서 spring security 일시정지 시킴
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ShareTourApplication {
 
