7a9da9caa99330bd3947d92aa79c7c957bbb23b9
cmthefox <jeonchangmin@gmail.com>
2023-10-10 15:40:50 +0900

    10.7. CM과 Merge

--- src/main/java/com/kosa/ShareTour/controller/UserController.java
+++ src/main/java/com/kosa/ShareTour/controller/UserController.java
@@ -1,4 +1,51 @@
 package com.kosa.ShareTour.controller;
 
+import com.kosa.ShareTour.dto.UserFormDto;
+import com.kosa.ShareTour.entity.User;
+import com.kosa.ShareTour.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+
+@RequestMapping("/users")
+@Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping(value = "/new")
+    public String userForm(Model model){
+        model.addAttribute("userFormDto",new UserFormDto());
+        return "user/userForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String newUser(@Valid UserFormDto userFormDto, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "User/UserForm";
+        }
+
+        try {
+            User user = User.createUser(userFormDto, passwordEncoder);
+            userService.saveUser(user);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "user/userForm";
+        }
+
+        return "redirect:/";
+    }
+
+
+
+
 }
--- src/main/java/com/kosa/ShareTour/dto/UserFormDto.java
+++ src/main/java/com/kosa/ShareTour/dto/UserFormDto.java
@@ -1,32 +1,50 @@
 package com.kosa.ShareTour.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.thymeleaf.spring5.processor.SpringErrorClassTagProcessor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class UserFormDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다")
     private String name;
 
+    @NotBlank(message = "이메일은 필수 입력 값입니다")
+    @Email(message = "이메일 형식으로 입력 해야됩니다")
     private String email;
 
+    @NotBlank(message = "닉네임은 필 수 입력 값입니다")
     private String nickname;
 
+    @NotBlank(message = "비밀번호는 필수 입력값입니다")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력 해야 합니다")
     private String password;
 
     private LocalDateTime createTime;
 
     private String imgUrl;
 
+    @NotBlank(message = "성별은 필수 입력 ")
     private String gender;
 
+    @NotBlank(message = "생년월일은 필수 입력값입니다")
+    @Length(min = 8, max = 8, message = "생년월일은 8자로 입력 해야 됩니다")
     private LocalDate birthday;
 
+    @NotBlank(message = "전화번호는 필수 입력값입니다")
+    @Length(min = 8, max= 8, message = "전화번호는 '-'없이 11자 이하로 입력 해야 됩니다")
     private String phone;
 
     private String address;
 
     private String grade;
+
 }
