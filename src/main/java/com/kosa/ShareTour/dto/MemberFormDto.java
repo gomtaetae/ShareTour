37edb2b51317e49327d00ff87a68e09cf16bd4ad
cmthefox <jeonchangmin@gmail.com>
2023-10-13 10:01:22 +0900

    Accommodation 엔티티의 일부 매핑 오류로 DB상(MySql)에서 테이블 삭제 불가 오류 확인 -> 수정완료.

--- src/main/java/com/kosa/ShareTour/dto/MemberFormDto.java
+++ src/main/java/com/kosa/ShareTour/dto/MemberFormDto.java
@@ -6,9 +6,6 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Data
 public class MemberFormDto {
 
