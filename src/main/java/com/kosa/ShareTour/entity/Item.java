5dcd2e869f2cef7d93acf9a7707c57b63b06f7bd
gomtaetae <rlaxodls108@hanmail.net>
2023-10-13 15:29:06 +0900

    상품등록까지 정상작동 전체 코드 취합완료

---
+++ src/main/java/com/kosa/ShareTour/entity/Item.java
@@ -0,0 +1,60 @@
+package com.kosa.ShareTour.entity;
+
+import com.kosa.ShareTour.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item extends BaseEntity{
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;       //패키지 이름
+
+    @Column(name = "content", columnDefinition = "LONGTEXT", nullable = false)
+    private String content;       //패키지 설명
+
+    @Column(name="img", nullable = false)
+    private String img;
+
+    @Column(name="totalprice", nullable = false)
+    private Integer totalPrice;
+
+    @Column(name="in_stock", nullable = false)
+    private Integer inStock;
+
+    @Column(name="stock_left")
+    private Integer stockLeft;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;    //패키지 판매 상태
+
+//    @ManyToOne
+//    @JoinColumn(name="places_id")
+//    private Place place;
+//
+//    @ManyToOne
+//    @JoinColumn(name="landmarks_id")
+//    private Landmark landmark;
+//
+//    @ManyToOne
+//    @JoinColumn(name="accommodations_id")
+//    private Accommodation accommodation;
+//
+//    @ManyToOne
+//    @JoinColumn(name="restaurants_id")
+//    private Restaurant restaurant;
+}
